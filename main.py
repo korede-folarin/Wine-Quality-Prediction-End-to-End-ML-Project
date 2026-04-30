c1b8067de376d01cdaab999a4cc12a59ab77b0db
Folarin Korede Mary <folarinkoredemary@gmail.com>
2026-04-30 03:08:03 +0100

    Data Validation Pipeline

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.datascience import logger
 from src.datascience.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
+from src.datascience.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
 
 logger.info('Welcome to the Data Science Project Template!')
 
@@ -14,3 +15,16 @@ if __name__ == '__main__':
     except Exception as e:
         logger.exception(e)
         raise e
+    
+
+
+# Stage 2 - Data Validation
+STAGE_NAME = 'Data Validation Stage'
+try:
+    logger.info(f'>>>>>> stage {STAGE_NAME} started <<<<<<')
+    obj = DataValidationTrainingPipeline()
+    obj.main()
+    logger.info(f'>>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x')
+except Exception as e:
+    logger.exception(e)
+    raise e
