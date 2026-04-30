a2831e16093f15f01820f199edc4f8e8041c6b37
Folarin Korede Mary <folarinkoredemary@gmail.com>
2026-04-30 04:18:35 +0100

    Data Transformation Pipeline

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.datascience import logger
 from src.datascience.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 from src.datascience.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
+from src.datascience.pipeline.data_transformation_pipeline import DataTransformationTrainingPipeline
 
 logger.info('Welcome to the Data Science Project Template!')
 
@@ -24,6 +25,17 @@ try:
     logger.info(f'>>>>>> stage {STAGE_NAME} started <<<<<<')
     obj = DataValidationTrainingPipeline()
     obj.main()
+    logger.info(f'>>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x')
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Data Transformation Stage"
+try:
+    logger.info(f'>>>>>> stage {STAGE_NAME} started <<<<<<')
+    data_transformation = DataTransformationTrainingPipeline()
+    data_transformation.main()
     logger.info(f'>>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x')
 except Exception as e:
     logger.exception(e)
